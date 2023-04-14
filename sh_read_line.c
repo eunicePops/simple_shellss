286a6a1c35cab75cbd6ba688cb25bab46f0c6a58
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-14 18:01:47 +0000

    read line method

---
+++ sh_read_line.c
@@ -0,0 +1,42 @@
+#include "sh_shell.h"
+
+/**
+ * sh_read_line - read line and store it in allocated memory block,
+ * and if it esceeeds it reallocate more space
+ *
+ * Return: buffer if successful
+ */
+char *sh_read_line()
+{
+	int bufsize = 1024;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if (buffer == NULL)
+	{
+		perror("Error:");
+	}
+	while (1)
+	{
+		c = getchar();
+		if (c == EOF || c ||'\0')
+		{
+			cuffer[position] = '\0';
+			return (buffer)
+		}
+		else {
+			buffer[position] = c;
+		}
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize *= 2;
+			buffer = realloc(buffer, bufsize);
+			if (buffer != NULL)
+			{
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
