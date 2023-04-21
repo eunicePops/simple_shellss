a0ac40a7acfa704ebb9cadd8ef5ed4b4179d6247
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-21 10:22:24 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 10
+#define TOK_DELIM " \t\r\n\a"
+
+
+char* fget(char* s, int n, FILE* stream);
+void parse_command(char* command, char** arguments);
+int launch(char** arguments);
+int execute(char **args);
+
+int num_builtins();
+
+int sh_cd(char **args);
+int sh_help(__attribute__((unused))char **args);
+int sh_exit(__attribute__((unused))char **args);
+int env(char **args);
+
+#endif
