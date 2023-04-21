8c5e07b85bce0fcaa75e34a52693fc2fe50b03e6
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-21 10:29:45 +0000

    launch.c

---
+++ launch.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int launch(char** arguments) {
+	pid_t pid;
+	
+	pid = fork();
+	if(pid < 0) {
+		printf("Error: failed to fork child process\n");
+		exit(1);
+	} else if(pid == 0) {
+		if(execvp(arguments[0], arguments) < 0) {
+			printf("./shell: No such file or directory\n");
+			exit(1);
+		}
+	} else {
+		wait(NULL);
+	}
+	return (1);
+}
