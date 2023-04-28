91a940e2d339c212fb308df1e6c78a2f9dd9abb6
eunicePops <enablersolutions@gmail.com>
2023-04-28 15:32:35 -0700

    fix

--- main.h
+++ main.h
@@ -12,6 +12,50 @@
 #define TOK_DELIM " \t\r\n\a"
 extern char **environ;
 
+/**
+ *struct infopass - this struct contains information to be passed into a function
+ *@arg: a generated string
+ *@argv: an array of strings generated from arg
+ *@path: a path for the current command of string 
+ *@argc: the count of the arguments 
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
+ *@fname: the program filename
+ *@env: linked list local copy of environ
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
 char *fget(char *s, int n, FILE *stream);
 void parse_command(char *command, char **args);
 int launch(char **args);
